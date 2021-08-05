7c71a3f896d7357966a0fd2904bdfa130b4e1056
Yoshito Osanai <yoshito-osanai@mes.co.jp>
2021-08-05 17:54:34 +0900

    2

---
+++ operator.js
@@ -0,0 +1,11 @@
+class Operator{
+    constructor(name, rarelity) {
+        this.name = name
+        this.rarelity = rarelity
+        this.ad = 
+        this.ar = ar
+    }
+}
+
+let mizuki = new Operator('mizuki', 6)
+console.log(mizuki)
