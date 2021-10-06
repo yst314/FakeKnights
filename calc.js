550d96461bcfb0526fdc70421452726fec11a95c
Yoshito Osanai <yoshito-osanai@mes.co.jp>
2021-10-06 12:23:48 +0900

    refactoring

--- calc.js
+++ calc.js
@@ -12,7 +12,7 @@ surtr = {
     magic: 100,
     true: 0,
     talent1: function () {
-        this.penetrateRes = 22;
+        this.penetrateRes += 22;
     },
     skill3: function () {
         this.atk += Math.round(this.baseAtk * 330 / 100);
@@ -91,63 +91,93 @@ walfarin = {
 a = {
     skill3: function (target) {
         target.atk += Math.floor(target.baseAtk * 50 / 100);
-        target.as -= target.baseAspd * 50 / 100;
+        target.aspd -= target.baseAspd * 50 / 100;
     }
 }
 
-testE = {
-    hp: 100000,
-    def: 100,
-    res: 0,
-    totalDamageTaken: 0,
-    takeDamage: function (damage) {
+enemy = {
+    hexedColossus : {
+        atk: 3600,
+        aspd: 7,
+        hp: 150000,
+        recovery: 0,
+        def: 800,
+        res: 40,
+        weight: 5,
+        ms: 0.4,
+        range: 0,
+        stun: false,
+        silence: false,
+    },
+    mudrockColossus : {
+        type: 'Elite',
+        atk: 3000,
+        aspd: 7,
+        hp: 100000,
+        def: 700,
+        res: 30,
+    },
+    
+    testE : {
+        hp: 1000,
+        def: 100,
+        res: 0,
+    }
+}
+
+class enemyUnit{
+    constructor(status) {
+        this.defaultStatus = status;
+        this.setStatus(status);
+    }
+
+    setStatus(status) {
+        this.hp = status.hp;
+        this.def = status.def;
+        this.res = status.res;
+        this.totalDamageTaken = 0;
+    }
+
+    takeDamage(damage) {
         this.hp -= damage;
         this.totalDamageTaken += damage;
     }
+
+    reset() {
+        this.setStatus(this.defaultStatus)
+    }
 }
 
-hexedColossus = {
-    atk: 3600,
-    aspd: 7,
-    hp: 150000,
-    recovery: 0,
-    def: 800,
-    res: 40,
-    weight: 5,
-    ms: 0.4,
-    range: 0,
-    stun: false,
-    silence: false,
+mudrockColossus = new enemyUnit(enemy.mudrockColossus)
+hexedColossus = new enemyUnit(enemy.hexedColossus)
+
+let duration = 15;
+
+function attackTimes (duration, unit) {
+    return Math.floor(duration / unit.aspd);
 }
 
-mudrockColossus = {
-    type: 'Elite',
-    atk: 3000,
-    aspd: 7,
-    hp: 100000,
-    def: 700,
-    res: 30,
+function calcDamage (attacker, defender, duration){
+    for (let i = 0; i < attackTimes(duration, attacker); i++) {
+        let attack = attacker.dealAttack();
+        let damage = attack.calcDamage(defender);
+        defender.takeDamage(damage);
+    }
+    return defender.totalDamageTaken
 }
 
 exusiai.talent1();
 exusiai.talent2();
-walfarin.skill2(exusiai);
-a.skill3(exusiai);
+//walfarin.skill2(exusiai);
+//a.skill3(exusiai);
 exusiai.skill3();
 
-let duration = 15;
-times = function (unit) {
-    return Math.floor(duration / unit.aspd);
-}
-
-for (let i = 0; i < times(exusiai); i++) {
-    let attack = exusiai.dealAttack();
-    testE.takeDamage(attack.calcDamage(mudrockColossus));
-}
-
-console.log(testE);
+console.log(calcDamage(exusiai, mudrockColossus, duration));
+mudrockColossus.reset()
 
 surtr.talent1()
 surtr.skill3()
+walfarin.skill2(surtr);
 
-console.log(surtr.atk)
+console.log(calcDamage(surtr, mudrockColossus, duration));
+mudrockColossus.reset()
